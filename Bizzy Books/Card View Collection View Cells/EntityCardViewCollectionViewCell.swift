da8303f1531f256784d34ccf21918296103b41c1
caldwbr <caldwbr@gmail.com>
2017-12-13 21:32:52 -0600

    A lot of layout work on EntityCardViewCollectionViewCell dot xib and dot swift

--- Bizzy Books/Card View Collection View Cells/EntityCardViewCollectionViewCell.swift
+++ Bizzy Books/Card View Collection View Cells/EntityCardViewCollectionViewCell.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2017 Caldwell Contracting LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class EntityCardViewCollectionViewCell {
+    
+    @IBOutlet weak var entityCardViewNameLabel: UILabel!
+    @IBOutlet weak var entityCardViewLineBreakView: UIView!
+    @IBOutlet weak var entityCardViewPhoneNumberView: UIView!
+    @IBOutlet weak var entityCardViewPhoneNumberLabel: UILabel!
+    @IBOutlet weak var entityCardViewEmailView: UIView!
+    @IBOutlet weak var entityCardViewEmailLabel: UILabel!
+    @IBOutlet weak var entityCardViewAddressView: UIView!
+    @IBOutlet weak var entityCardViewStreetLabel: UILabel!
+    @IBOutlet weak var entityCardViewCityStateLabel: UILabel!
+    @IBOutlet weak var entityCardViewSSNLabel: UILabel!
+    @IBOutlet weak var entityCardViewEINLabel: UILabel!
+    
+}
