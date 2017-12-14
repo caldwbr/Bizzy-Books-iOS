d9e57944b7429331d782f99d9d852d30365d6679
caldwbr <caldwbr@gmail.com>
2017-12-13 19:47:47 -0600

    More CardView work

---
+++ Bizzy Books/Card View Collection View Cells/EntityCardViewCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  EntityCardViewCollectionViewCell.swift
+//  Bizzy Books
+//
+//  Created by Brad Caldwell on 12/13/17.
+//  Copyright © 2017 Caldwell Contracting LLC. All rights reserved.
+//
+
+import Foundation
